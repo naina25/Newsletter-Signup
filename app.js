2bdd51016c41d2ba135522d609abdbdd5b9824bd
naina <nanu02d8@gmail.com>
2020-10-27 18:05:45 +0530

    Refactored code of failure page

--- app.js
+++ app.js
@@ -41,10 +41,10 @@ app.post('/', (req,res) => {
 
     const request = https.request(url, options, function(response){
         if(response.statusCode === 200){
-            res.sendFile(__dirname + "/failure.html")
+            res.sendFile(__dirname + "/success.html")
         }
         else{
-            res.sendFile(__dirname + "/success.html")
+            res.sendFile(__dirname + "/failure.html")
         }
 
        response.on("data", function(data){
@@ -54,6 +54,11 @@ app.post('/', (req,res) => {
     request.write(jsonData);
     request.end();
 });
+
+app.post("/failure", (req, res) =>
+{
+    res.redirect("/");
+})
 
 app.listen('5000',() => {
     console.log("Server has started on port 5000");
