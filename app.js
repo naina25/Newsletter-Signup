82fc419002e45b828695325d44ae305ee89e726e
naina <nanu02d8@gmail.com>
2020-10-27 16:17:19 +0530

    Started sending login details from client side to Mailchimps' server

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-
+const https = require('https');
+require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -15,6 +16,36 @@ app.post('/', (req,res) => {
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
     const email = req.body.email;
+
+    const data = {
+        members : [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data);
+    const listID = process.env.LIST_ID;
+    const authKey = process.env.API_KEY;
+    const url = `https://us2.api.mailchimp.com/3.0/lists/${listID}`
+    const options = {
+        method: "POST",
+        auth: `naina:${authKey}`
+    }
+
+    const request = https.request(url, options, function(response){
+       response.on("data", function(data){
+           console.log(JSON.parse(data));
+       })
+    })
+    request.write(jsonData);
+    request.end();
 });
 
 app.listen('5000',() => {
