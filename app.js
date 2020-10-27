6f0181c483af3da1db1d6bf5f7b73abe13ead99d
naina <nanu02d8@gmail.com>
2020-10-27 14:50:36 +0530

    Started fetching users' data from signup form

--- app.js
+++ app.js
@@ -4,14 +4,18 @@ const request = require('request');
 
 
 const app = express();
-
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 app.get('/', (req,res) => {
     res.sendFile(__dirname + "/signup.html")
 });
 
+app.post('/', (req,res) => {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+});
 
 app.listen('5000',() => {
     console.log("Server has started on port 5000");
